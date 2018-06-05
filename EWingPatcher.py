5ed0d11054f318def541966feaf2f50e442dfd06
Eagleheardt <Eagleheardt@users.noreply.github.com>
2018-06-04 23:11:32 -0500

    Reworked backend, added rename function
    reworked the backend
     - made it more object oriented
     - more extensible
     - more comments
    
    rename function allows the user to automatically append the name of the patch ontot he file

--- EWingPatcher.py
+++ EWingPatcher.py
@@ -1,50 +1,228 @@
 #!/usr/bin/python3
 
-# Developed by Eagleheardt (2017)
-# Inspired by Xezlec (2017) ips.py
+# Developed by Eagleheardt (2017 - 2018)
 # see also https://github.com/Eagleheardt
 
 # This is hereby released into the public domain
-# please credit our work, if you use it
 
-from tkinter import *
-from tkinter import ttk
-from tkinter.ttk import *
-from tkinter import filedialog
-from PIL import Image
-from PIL import ImageTk
+# Please credit me, if you use any parts of this!
 
-root=Tk() # gui window is called root
-root.title("E-Wing IPS Patcher") # set the title of the window
-root.geometry("220x230")
+from tkinter import * # imports the TKinter library
+from tkinter import ttk # imports the TKinter nicer buttons
+from tkinter.ttk import * # imports the TKinter nicer buttons
+from tkinter import filedialog # imports the TKinter filedialog popup interface
+import os # imports low level OS functions
 
-# Global variable declaration
+########################################################################################
+########################################################################################
 
-ROMPath = StringVar() # Path of ROM file
-IPSPath = StringVar() # Path of IPS file
+#################### Global Variable Declaration ####################
+
+########################################################################################
+########################################################################################
+
+mainWindow=Tk() # gui window is called mainWindow
+mainWindow.title("E-Wing IPS Patcher") # set the title of the window
+mainWindow.geometry("400x280") # main window starts off this size
+
+fullROMPath = StringVar() # Path of ROM file
+fullPatchPath = StringVar() # Path of patch file
 ROMinf = StringVar() # Display of ROM path
-IPSinf = StringVar() # Display of IPS path
+patchInf = StringVar() # Display of patch path
 
-ROMinf.set("No ROM Selected")
-IPSinf.set("No IPS Selected")
+changeNameEnabled = IntVar() # checks if the user wants to rename the file (0=no, 1=yes)
 
-# this mess sets the tray icon to my E-Wing logo
-iconImg = Image.open("Icon.bmp")
-iconPhoto = ImageTk.PhotoImage(iconImg)
-root.tk.call('wm', 'iconphoto', root._w, iconPhoto)
+fullROMPath.set("") # sets ROM path to empty string
+fullPatchPath.set("") # sets patch path to empty string
+ROMinf.set("No ROM Selected") # Sets default message for the ROM
+patchInf.set("No patch Selected") # Sets default message for the patch
+
+aboutString = (
+"""
+If the "Append patch name" box
+is checked, the name of the patch
+will be added to the name of the ROM
+when patching occurs.\n
+It is free, hopefully easy to use,
+and open source!\n
+The comments in the code detail
+the functions.\n
+See the IPS and BPS file type specifications for more information!\n
+This utility was created by Eagleheardt
+"""
+) # Information about myself and the program ###########################################
+
+########################################################################################
+########################################################################################
+
+#################### Generic Method Declarations ####################
+
+########################################################################################
+########################################################################################
+
+# opens a window for information/errors
+
+def openWindow(titleText="Window Title", bodyText="Body Text"):
+
+	infoWin = Toplevel()
+	infoWin.title(titleText)
+	infoMsg = Message(infoWin,text=bodyText,width=300).pack(padx=10,pady=5)
+	infoBtn = Button(infoWin, text="Close",command=infoWin.destroy).pack(padx=10,pady=10)
+
+	return # exits openWindow(titleText="Window Title", bodyText="Body Text") function
+
+# sets the ROM path
+
+def setROMPath():
+
+	# brings up a filedialog to select the ROM file
+	# sets the title of the window to "Select ROM file"
+	r = filedialog.askopenfilename(title = "Select ROM file")
+
+	# sets the ROM path to the global variable
+	fullROMPath.set("{0}".format(r))
+
+	return # exits setROMPath() function
+
+# sets the patch patch
+
+def setPatchPath():
+
+	# brings up a filedialog to select the IPS file
+	# sets the title of the window to "Select IPS file"
+	p = filedialog.askopenfilename(title = "Select IPS file")
+
+	# sets the IPS path to the global variable
+	fullPatchPath.set("{0}".format(p))
+
+	return # exits setPatchPath() function
+
+# gets int position of final right slash in a given string
+
+def lastSlash(someString):
+	
+	return someString.rfind("/")
+
+# rename the file
+
+def fileRename(oFilePath, patchName, newName=""):
+		
+	dotNdex = oFilePath.rfind(".") # finds the last dot
+	f = ("{0}".format(oFilePath)) # oFilePath set to f
+	d = f[dotNdex:] # d is the file extension
+
+	if newName == "":
+		# if no name specified
+
+		# p is the whole file path of the original file
+		# concatinated with the phrase " patched with "
+		# followed by the name of the patch
+		# and the file extension of the original file
+		p = ("{0} patched with {1}.{2}".format(oFilePath,patchName,d))
+
+		# replaces the original file with the newly formatted one
+		os.replace(oFilePath, p)
+	else:
+		# if there is a custom name specified
+
+		k = lastSlash(oFilePath) # gets the index of the last slash in the file
+		filePath = ("{0}".format(oFilePath[:k])) # formats the file path
+		
+		# holds the new file name with the original file extension
+		p = ("{0}{1}.{2}".format(oFilfilePathePath,newName,d))
+
+		# replaces the original file with the newly formatted one
+		os.replace(oFilePath, p)
+
+	return # exits fileRename(oFilePath, patchName, newName="") function
+
+# Resets the global variables and the labels on the main window
+
+def resetMainWindow():
+
+	fullROMPath.set("")
+	fullPatchPath.set("")
+
+	ROMinf.set("No ROM Selected")
+	patchInf.set("No patch Selected")
+	return # exits resetMainWindow() function
+
+# Checks the ROM makes sure it can be open for reading in byte mode
+
+def checkROM(checkfullROMPath):
+
+	try:
+		# tries to open the ROM
+		checkROMIO = open(checkfullROMPath,'r+b')
+	except:
+		# catches any error with the ROM opening
+		# opens a window notifying of the error
+		
+		openWindow("CrItIcAl ErRoR!", "Unable to read ROM file.")
+		fullROMPath.set("") # sets the global variable to empty string
+		ROMinf.set('Unsupported ROM!') # sets the information label to unsupported ROM
+	else:
+		# if no exception happens this block goes off
+
+		k = lastSlash(checkfullROMPath) # gets the position of the last slash in the path
+		fullROMPath.set(checkfullROMPath) # sets the global variable to the path directed in
+		ROMinf.set("..." + checkfullROMPath[k:]) # sets the label to ".../ROM_NAME"
+	finally:
+		# this block fires no matter what
+
+		checkROMIO.close() # closes the file
+		return # exits the checkROM(checkfullROMPath) function
+
+def checkPatch(checkPatchPath):
+	# does basic file checking for patch files
+
+	try:
+		checkPatchFile = open(checkPatchPath,'rb') # opens patch file
+
+		if checkPatchFile.read(5) != b'PATCH': # IPS patches start with the word "PATCH"
+			raise LookupError('Invalid Patch') # triggers an error
+
+		else:
+			k = lastSlash(checkPatchPath) # gets the position
+			fullPatchPath.set(checkPatchPath) # sets full patch path
+			patchInf.set("..." + checkPatchPath[k:]) # sets the label to ".../IPS_NAME"
+		
+	except:
+		# if an error occurs
+ 	
+		openWindow("CrItIcAl ErRoR!", "Unable to read patch file.")
+		patchInf.set("Unidentified patch!") # sets the label to "Unidentified patch!"
+		fullPatchPath.set("") # sets the global variable to empty string
+	finally:
+		checkPatchFile.close() # closes patch file
+
+		return # exits checkPatch(checkPatchPath) function
+
+########################################################################################
+########################################################################################
+
+#################### IPS Method Declarations ####################
+
+########################################################################################
+########################################################################################
 
 # reads records of IPS file
-def readRecord(patchFile):
-	# 5 byte header
-	# First 3 bytes are the offset in the ROM file 
-	# Where the information will be inserted
 
-	# Second 2 bytes are the 'length' of the following data
+def readIPSRecord(patchFile):
+	# IPS files have a 5 byte header, per record
+
+	# First 3 bytes of the header are the offset in the ROM file 
+	# Where the information will be inserted
+	
 	first3 = patchFile.read(3)
+
 	# if the first 3 bytes say 'EOF' we've reached the end of the file
 	if first3 == b'EOF':
-		raise ValueError('END OF FILE')
+		raise EOFError('END OF FILE')
+		# This error will serve as the terminating condition of a loop later
 	offset = int.from_bytes(first3,byteorder='big')
+
+	# The last 2 bytes of the header are the 'length' of the following data
 	loadLength = int.from_bytes(patchFile.read(2),byteorder='big')
 
 	# If the loadLength is 0, then the next record is an 'RLE' style record
@@ -61,79 +239,29 @@ def readRecord(patchFile):
 		replacement = patchFile.read(loadLength)
 		return offset, replacement
 
-# Opens a window for information/errors
+# patches a ROM with an IPS patch
 
-def openWindow(titleText="Window Title", bodyText="Body Text"):
-	infoWin = Toplevel()
-	infoWin.title(titleText)
-	infoMsg = Message(infoWin,text=bodyText,width=300).pack(padx=10,pady=5)
-	infoBtn = Button(infoWin, text="Close",command=infoWin.destroy).pack(padx=10,pady=10)
-	return
+def patchIPSFile(aROM,aPatch):
+	# Open the files
 
-# Button execution methods
-
-def btnROMClick():
-	# only does basic file checking for ROM
-
-	checkROMPath = filedialog.askopenfilename() # brings up a filedialog to select the ROM
-	try:
-		checkROMIO = open(checkROMPath,'r+b')
-	except:
-		openWindow("CrItIcAl ErRoR!", "Unable to read ROM file.")
-		ROMinf.set('Unsupported ROM!')
-		return
-	checkROMIO.close()
-	ROMPath.set(checkROMPath)
-	ROMinf.set(checkROMPath[-35:])
-	return
-
-def btnIPSClick():
-	# only does basic file checking for IPS
-	checkIPSPath = filedialog.askopenfilename() # brings up a filedialog to select the IPS
-	try:
-		# IPS patches start with the word "PATCH"
-		checkIPSIO = open(checkIPSPath,'rb')
-		if checkIPSIO.read(5) != b'PATCH':
-			raise ValueError('IPS not supported')
-	except:
-		openWindow("CrItIcAl ErRoR!", "Unable to read IPS file.")
-		IPSinf.set("Unsupported IPS!")
-		return
-		
-	checkIPSIO.close()
-	IPSPath.set(checkIPSPath)
-	IPSinf.set(checkIPSPath[-35:])
-	return
-
-def btnApplyClick():
+	finalROM = open(aROM,'r+b')
+	finalIPS = open(aPatch,'rb')
+	finalIPS.seek(5) # Moves the pointer 5 bytes, past the string "PATCH"
 	
-	# Open our files
-	finalROM = open(ROMPath.get(),'r+b')
-	finalIPS = open(IPSPath.get(),'rb')
-	finalIPS.seek(5)
-	# Moves the pointer 5 bytes, past the word "PATCH"
-
-	# Do the patching!
-	records = 0
-	dataWritten = 0
-	try:
+	records = 0 # number of times data is written
+	dataWritten = 0 # amount of data that was inserted into the ROM
+	try:	
 		while True:
-			offset,replacement = readRecord( finalIPS )
-			# this will throw a value error at the end of file marker, kicking us out of the loop
+			offset,replacement = readIPSRecord(finalIPS) # reads a record
 
-			records += 1
-			dataWritten += len( replacement )	
-			# each loop increases the number of writes
-			# and adds the amount of replacement each time
-
-			finalROM.seek( offset )	
-			# moves file pointer to the offset
-			finalROM.write( replacement )
-			# writes the replacement data to the offset location
+			records += 1 # increases the number of writes
+			dataWritten += len(replacement)	# adds the amount of write each time
+			
+			finalROM.seek(offset) # moves file pointer to the offset
+			finalROM.write(replacement) # writes the replacement data
 	except:
-		pass
-		# when the exception occurs, ignore it
-
+		pass # when the exception occurs, the loop is broken
+		
 	truncateOffset = finalIPS.read(3)
 	# checks 3 bytes past the EOF marker for a truncation length
 	if len(truncateOffset) != 0:
@@ -141,43 +269,121 @@ def btnApplyClick():
 		# if it exists, truncates the file to that size
 
 	# info window with changes made
-	openWindow("Attention!", "Records changed: {}. \nBytes changed: {}".format(records,dataWritten))
+	openWindow("Attention!", "Number of records changed: {} \n"
+				 "Number of bytes changed: {}".format(records,dataWritten))
 	
 	# close files
 	finalROM.close()
 	finalIPS.close()
-	
-	# Reset all variables/Labels
-	ROMPath.set("")
-	IPSPath.set("")
 
-	ROMinf.set("No ROM Selected")
-	IPSinf.set("No IPS Selected")
 	return
+
+########################################################################################
+########################################################################################
+
+#################### Button Execution Methods ####################
+
+########################################################################################
+########################################################################################
+
+# Click to select ROM file
+
+def btnROMClick():
+
+	setROMPath()
+	checkROM(fullROMPath.get())
+
+	return # exits btnROMClick() function
+
+# Click to select patch file
+
+def btnIPSClick():
+
+	setPatchPath()
+	checkPatch(fullPatchPath.get())
+
+	return # exits btnIPSClick() function
+
+# do the patching!
+
+def btnApplyClick():
+	
+	ROMCheck = fullROMPath.get()
+	
+	if ROMCheck == "":
+		# if no ROM set
+
+		openWindow("CrItIcAl ErRoR!","No ROM selected!")
+		return # exits btnApplyClick() function
+	else:
+		try:
+			# tries to check the path once more
+			checkPatch(fullPatchPath.get())
+		except:	
+			# if the ROM doest verify
+			openWindow("CrItIcAl ErRoR!","Unidentified patch!")
+		else:
+			# if the ROM does verify
+			p = fullPatchPath.get() # holds the patch path
+			r = fullROMPath.get()   # holds the ROM path
+
+			patchIPSFile(r,p) # calls the patch on the file
+		
+			if changeNameEnabled.get() == 1:
+				# if the user wants to rename the ROM
+				k = lastSlash(p)
+				nameOfPatch = p[k+1:]
+				fileRename(oFilePath=ROMCheck, patchName=nameOfPatch, newName="")
+		
+			resetMainWindow() # resets the varbiables and window
+		finally:
+
+			return # exits btnApplyClick() function
+
+# Displays information about my program
 
 def btnAbtClick():
-	openWindow("Attention!","This utility was created by Eagleheardt.\nInspired by Xezlec's original utility.\n\nThe comments in the code detail what it does\nas well as the specification of the IPS file.")
-	return
 
-# Button declaration and placement
+	openWindow("Attention!",aboutString)
+
+	return # exits btnAbtClick() function
+
+########################################################################################
+########################################################################################
+
+#################### Button Declaration and Placement ####################
+
+########################################################################################
+########################################################################################
+
+#checkbox to enable file renaming
+renameFileChecker = Checkbutton(mainWindow,
+			text="Append patch name",
+			variable=changeNameEnabled).pack(padx=5,pady=5,anchor=W)
 
 # Buttons and labels for ROM section
-btnROM = Button(root,text="Choose ROM",command=btnROMClick).pack(padx=5,pady=5,fill=X)
+btnROM = Button(mainWindow,text="Choose ROM",command=btnROMClick).pack(padx=5,pady=5,fill=X)
 Label(textvariable=ROMinf,justify=LEFT).pack(padx=5,pady=5,fill=X)
 
-# Buttons and labels for IPS section
-btnIPS = Button(root,text="Choose Patch",command=btnIPSClick).pack(padx=5,pady=5,fill=X)
-Label(textvariable=IPSinf,justify=LEFT).pack(padx=5,pady=5,fill=X)
+# Buttons and labels for patch section
+btnPatch = Button(mainWindow,text="Choose Patch",command=btnIPSClick).pack(padx=5,pady=5,fill=X)
+Label(textvariable=patchInf,justify=LEFT).pack(padx=5,pady=5,fill=X)
 
 # Apply, About, Close buttons
-btnApply = Button(root,text="Apply Patch",command=btnApplyClick).pack(padx=5,pady=5,fill=X)
-btnAbt = Button(root,text="About",command=btnAbtClick).pack(padx=5,pady=5,fill=X)
-btnClose = Button(root,text="Close",command=root.destroy).pack(padx=5,pady=5,fill=X)
+btnApply = Button(mainWindow,text="Apply Patch",command=btnApplyClick).pack(padx=5,pady=5,fill=X)
+btnAbt = Button(mainWindow,text="About",command=btnAbtClick).pack(padx=5,pady=5,fill=X)
+btnClose = Button(mainWindow,text="Close",command=mainWindow.destroy).pack(padx=5,pady=5,fill=X)
 
-root.mainloop() # this starts the GUI
+########################################################################################
+########################################################################################
+
+mainWindow.mainloop() ##### This Starts the GUI #####
+
+########################################################################################
+########################################################################################
 
 # resources:
+# http://effbot.org/tkinterbook/
 # https://zerosoft.zophar.net/ips.php
 # http://www.smwiki.net/wiki/IPS_file_format
 # http://justsolve.archiveteam.org/wiki/IPS_(binary_patch_format)
-# Accessed 4/30/2017 - 11am CST
